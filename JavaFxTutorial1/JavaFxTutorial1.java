cc778fadb9279f291518da5fae0f311abfcac7ad
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-09-03 18:09:18 -0400

    First JavaFxTutorial!

--- JavaFxTutorial1/JavaFxTutorial1.java
+++ JavaFxTutorial1/JavaFxTutorial1.java
@@ -1,19 +1,25 @@
-/*
-imports will probably be done for you by the IDE.
-*/
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 /*
-Welcome! THis is JavaFx Tutorial 1.
+ Welcome! THis is JavaFx Tutorial 1.
 
-Every JavaFX application's main class must extend the class Application.
-It provides an abstract method start(), in which the stage (window) and scene (main background) will be initialized. Here is an example of a basic JavaFx application.
-*/
-
+ Every JavaFX application's main class must extend the class Application.
+ It provides an abstract method start(), in which the stage (window) and scene (main background) will be initialized. Here is an example of a basic JavaFx application.
+ */
 public class JavaFxTutorial1 extends Application {
-	@Override
-    public void start(Stage primaryStage) {
-    	// Create a new button
-        Button button = new Button();
+
+    @Override
+    public void start(Stage stage) {
+        // Create a new button
+        final Button button = new Button();
         // set the text of the button to "Click me!"
         button.setText("Click Me!");
         // add an event handler for when an action is performed on the button.
@@ -23,25 +29,26 @@ public class JavaFxTutorial1 extends Application {
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                button.setText("Hello World!");
             }
         });
 
         // Here is an example of the above in Java 8 syntax.
         /*
-		btn.setOnAction((event) -> System.out.println("Hello World!"))
-        */
+         btn.setOnAction((event) -> System.out.println("Hello World!"))
+         */
+        // When we click the button, it sets the button's text to "Hello World!"
         
         // A stackpane is a basic container which centers it's children.
         StackPane root = new StackPane();
         // Now add the button to the StackPane's children.
         root.getChildren().add(button);
-        
+
         // A scene is the main background for a JavaFx application.
         // Here we are making a new scene, with the root node as the stackpane, and
         // with a size of 300 x 300
         Scene scene = new Scene(root, 300, 300);
-        
+
         // Set the title of the window.
         stage.setTitle("Hello World!");
         // Set the scene of the stage. All stages have a scene to show when application starts.
@@ -55,14 +62,13 @@ public class JavaFxTutorial1 extends Application {
     // launch(args)
     // method from the Application class in main.
     public static void main(String[] args) {
-    	launch(args);
+        launch(args);
     }
 
     /*
-	This creates a basic JavaFx program.
-	Note that if you are using Eclipse, you will need the e(fx)clipse plugin.
-	If you are using netbeans, you can create a new JavaFx project from the new project window.
-	I don't know about IntelliJ.
-    */
+     This creates a basic JavaFx program.
+     Note that if you are using Eclipse, you will need the e(fx)clipse plugin.
+     If you are using netbeans, you can create a new JavaFx project from the new project window.
+     I don't know about IntelliJ.
+     */
 }
-
